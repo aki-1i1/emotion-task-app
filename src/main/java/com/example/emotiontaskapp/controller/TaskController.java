e459b00a1be3dd30ab84b7670d83a0b259bb7541
aki-1i1 <kokitaaki@yahoo.co.jp>
2026-05-27 15:50:42 +0900

    implement task add feature

--- src/main/java/com/example/emotiontaskapp/controller/TaskController.java
+++ src/main/java/com/example/emotiontaskapp/controller/TaskController.java
@@ -1,13 +1,24 @@
-package controller;
+package com.example.emotiontaskapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
 public class TaskController {
+
+    private List<String> tasks = new ArrayList<>();
+
     @GetMapping("/tasks")
     public List<String> getTasks() {
-        return List.of("タスク1", "タスク2");
+        return tasks;
+    }
+
+    @PostMapping("/tasks")
+    public List<String> addTask(@RequestBody String task) {
+        tasks.add(task);
+        return tasks;
     }
 }
